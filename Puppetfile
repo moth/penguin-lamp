7dbb4551468973758572038f83667d9eaae1cfe8
Mike Thomas <mike@mauva.co.uk>
2019-01-01 10:29:58 +0000

    Added gem

---
+++ Puppetfile
@@ -0,0 +1,3 @@
+forge 'http://forge.puppetlabs.com'
+
+mod 'puppetlabs/stdlib', '5.1.0'
--- manifests/package_gem.pp
+++ manifests/package_gem.pp
@@ -6,3 +6,8 @@ package { 'bundler':
   ensure   => installed,
   provider => gem,
 }
+
+package { 'generate-puppetfile':
+  ensure   => installed,
+  provider => gem,
+}
