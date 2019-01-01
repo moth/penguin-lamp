c59c502619d80015b9c9558f06bddf1d106e935f
Mike Thomas <mike@mauva.co.uk>
2019-01-01 13:41:42 +0000

    install apache

---
+++ manifests/module_apache.pp
@@ -0,0 +1,31 @@
+# include apache
+
+class { 'apache':
+  default_vhost => false,
+}
+
+apache::vhost { 'moth.zapto.org':
+  port          => '80',
+  docroot       => '/var/www/cat-pictures',
+  docroot_owner => 'www-data',
+  docroot_group => 'www-data',
+}
+
+apache::vhost { 'moth2.zapto.org':
+  port          => '80',
+  docroot       => '/var/www/cat-pictures2',
+  docroot_owner => 'www-data',
+  docroot_group => 'www-data',
+}
+
+file { '/var/www/cat-pictures/index.html':
+  content => "<img src='http://bitfieldconsulting.com/files/happycat.jpg'>",
+  owner   => 'www-data',
+  group   => 'www-data',
+}
+
+file { '/var/www/cat-pictures2/index.html':
+  content => "hello two",
+  owner   => 'www-data',
+  group   => 'www-data',
+}
